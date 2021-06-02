b164b612a262e38d8f96434198c18fa6f331391d
Viktorvrb <viktor.vorobiev@gmail.com>
2021-06-02 17:26:24 +0300

    Добавлена проверка на целое значение параметров, добавлен конструктор Error, диапазон не включая max, изменен способ работы функции если первый аргумент-max,функции заменены на стрелочные

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-function getRandomIntRound(min, max) {
-  if (min>=0 && max>min){
-    min = Math.round(min);//<--фрагмент кода взят https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    max = Math.round(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;//<--
-  } else {
-    return 'неверные значения диапазона';
+const getRandomIntRound=(min, max)=>{
+  if (min%1!==0 && max%1!==0){
+    throw new Error ('Only Int Numbers Allowed As The Parameters');
   }
-}
+  if (min>max){
+    const swap = max;
+    max =min;
+    min =swap;
+  }
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 getRandomIntRound();
-function getRandomIntFloat(min, max, dig) {
-  if (min>=0 && max>min && dig>=0 && dig<20){
-    const num = (Math.random() * (max - min + 1)) + min;
+const getRandomIntFloat=(min, max, dig)=>{
+  if (min>=0 && dig>=0 && dig<20){
+    if (min>max){
+      const swap = max;
+      max =min;
+      min =swap;
+    }
+    const num = (Math.random() * (max - min)) + min;
     return +num.toFixed(dig);
   } else {
-    return 'неверные значения диапазона и количество знаков после запятой';
+    throw new Error ('Parameters invalid');
   }
-}
+};
 getRandomIntFloat ();
